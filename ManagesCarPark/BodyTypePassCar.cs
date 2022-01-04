2386d782d4ca2fcfed78ca7beebe64dee98b0891
lax33 <lax33@tut.by>
2022-01-04 13:19:04 +0300

    refactoring

---
+++ ManagesCarPark/BodyTypePassCar.cs
@@ -0,0 +1,10 @@
+﻿namespace ManagesCarPark
+{
+    static class BodyTypePassCar
+    {
+        public static string sedan = "sedan";
+        public static readonly string hatchback = "hatchback";
+        public static readonly string crossover = "crossover";
+        public static readonly string jeep = "jeep";
+    }
+}
--- ManagesCarPark/Scooter.cs
+++ ManagesCarPark/Scooter.cs
@@ -1,35 +1,16 @@
-﻿using System;
-
-namespace ManagesCarPark
+﻿namespace ManagesCarPark
 {
     class Scooter : Car
     {
-        public Scooter (string automobileModel,
+        public Scooter(string automobileModel,
             string carType,
             string automobileColor,
             string automobileYear,
-            string vin,
             Engine engine,
             Transmission transmission,
             Chassis chassis) : base(automobileModel, carType, automobileColor, automobileYear,
-                                             vin, engine, transmission, chassis)
+            engine, transmission, chassis)
         {
-        }
-
-        public override void Print()
-        {
-            Console.WriteLine("Automobile model - " + automobileModel);
-            Console.WriteLine("Car type - " + carType);
-            Console.WriteLine("Color - " + automobileColor);
-            Console.WriteLine("Automobile Year - " + automobileYear);
-            Console.WriteLine("VIN - " + vin);
-            Console.WriteLine("Engine power - " + engine.power);
-            Console.WriteLine("Engine capacity - " + engine.capacity);
-            Console.WriteLine("Engine serial number - " + engine.serialNumber);
-            Console.WriteLine("Type transmission - " + transmission.typeTransmission);
-            Console.WriteLine("Manufacturer of transmission - " + transmission.manufacturer);
-            Console.WriteLine("Number of gears - " + transmission.numberOfGears);
-            Console.WriteLine();
         }
     }
 }
