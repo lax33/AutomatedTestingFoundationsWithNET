11a25693cf68804234d99717454ef28ffd228654
lax33 <lax33@tut.by>
2022-01-18 18:02:58 +0300

    1

--- Flight/Program.cs
+++ Flight/Program.cs
@@ -11,17 +11,4 @@ namespace Flight
         }
     }
 
-    //internal struct Coordinate
-    //{
-    //    public uint x;
-    //    public uint y;
-    //    public uint z;
-
-    //    public Coordinate(uint x, uint y, uint z)
-    //    {
-    //        this.x = x;
-    //        this.y = y;
-    //        this.z = z;
-    //    }
-    //}
 }
