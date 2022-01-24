18403680c1b141efa4ec4b2318bcc209ebe402d3
lax33 <lax33@tut.by>
2022-01-24 17:59:33 +0300

    1

--- Collections/ProgramCollections.cs
+++ Collections/ProgramCollections.cs
@@ -14,7 +14,7 @@ namespace Collections
                         new Engine(300, 3.5, TypeEnine.diesel, 4562121831),
                         new Transmission(TypeTransmission.automaticTransmission, 5, "Nissan"),
                         new Chassis(6, "656412", 15)),
-                    new PassengerCar("Ford", TypeCar.passengerCar, BodyTypePassCar.jeep, "rad", "2006",
+                    new PassengerCar("Ford", TypeCar.passengerCar, BodyTypePassCar.jeep, "White", "2006",
                         new Engine(160, 2.0, TypeEnine.gas, 798852123),
                         new Transmission(TypeTransmission.manualTransmission,6, "Renault"),
                         new Chassis(4, "985456", 2.5)),
@@ -22,7 +22,7 @@ namespace Collections
                         new Engine(300, 3.0, TypeEnine.diesel, 89235487),
                         new Transmission(TypeTransmission.manualTransmission, 4, Manufacturer.nissan),
                         new Chassis(6, "8521347", 25)),
-                    new Scooter("Renault", TypeCar.scuter, "Green", "2020",
+                    new Scooter("Minsk", TypeCar.scuter, "Green", "2020",
                         new Engine(15, 0.4, TypeEnine.gas, 45716334),
                         new Transmission(TypeTransmission.variableTransmission, 0, Manufacturer.renault),
                         new Chassis(2, "545486156", 0.15))
