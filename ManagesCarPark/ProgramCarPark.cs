12488dd3ae968cbea9d7e35ac71c29950471dd28
lax33 <lax33@tut.by>
2022-01-25 14:56:42 +0300

    refactoring the ProgramCarPark

--- ManagesCarPark/ProgramCarPark.cs
+++ ManagesCarPark/ProgramCarPark.cs
@@ -7,37 +7,30 @@ namespace ManagesCarPark
         static void Main(string[] args)
         {
             // Car Ford
-            Engine engineFord = new Engine(110, 1.6, TypeEnine.gas, 45625665489);
-            Transmission transmissionFord = new Transmission(TypeTransmission.automaticTransmission,
-                5, Manufacturer.ford);
-            Chassis chassisFord = new Chassis(4, "85214", 1.5);
-            PassengerCar passengerCarFord = new PassengerCar("Ford", TypeCar.passengerCar,
-                BodyTypePassCar.sedan, "red", "1969", engineFord, transmissionFord, chassisFord);
+            PassengerCar passengerCarFord = new PassengerCar("Ford", TypeCar.passengerCar, BodyTypePassCar.sedan, "red", "1969",
+                    new Engine(110, 1.6, TypeEnine.gas, 45625665489),
+                    new Transmission(TypeTransmission.automaticTransmission, 5, Manufacturer.ford),
+                    new Chassis(4, "85214", 1.5));
 
             // Truck Renault
-            Engine engineTruckRenault = new Engine(300, 3.0, TypeEnine.diesel, 89235487);
-            Transmission transmissionRenault = new Transmission(TypeTransmission.manualTransmission,
-                4, Manufacturer.nissan);
-            Chassis chassisRenault = new Chassis(6, "8521347", 25);
-            Truck truckRenault = new Truck("Renault", TypeCar.truck, TypeTruck.wagon, "blue",
-                "2012", engineTruckRenault, transmissionRenault, chassisRenault);
+            Truck truckRenault = new Truck("Renault", TypeCar.truck, TypeTruck.wagon, "blue", "2012",
+                new Engine(300, 3.0, TypeEnine.diesel, 89235487),
+                new Transmission(TypeTransmission.manualTransmission, 4, Manufacturer.nissan),
+                new Chassis(6, "8521347", 25));
 
 
             // Bus Nissan
-            Engine engineNissan = new Engine(220, 2.5, TypeEnine.diesel, 52348736915);
-            Transmission transmissionNissan = new Transmission(TypeTransmission.roboticTransmission,
-                6, Manufacturer.nissan);
-            Chassis chassisBus = new Chassis(6, "8215479325", 10);
-            Bus busNissan = new Bus("Nissan", TypeCar.bus,
-                "Yellow", "2006", "30", engineNissan, transmissionNissan, chassisBus);
+            Bus busNissan = new Bus("Nissan", TypeCar.bus, "Yellow", "2006", "30",
+                new Engine(220, 2.5, TypeEnine.diesel, 52348736915),
+                new Transmission(TypeTransmission.roboticTransmission, 6, Manufacturer.nissan),
+                new Chassis(6, "8215479325", 10));
 
 
             // Scuter Renault
-            Engine engineScuterRenault = new Engine(15, 0.4, TypeEnine.gas, 45716334);
-            Transmission transmissionScuterRenault = new Transmission(TypeTransmission.variableTransmission, 0, Manufacturer.renault);
-            Chassis chassisScuter = new Chassis(2, "545486156", 0.15);
             Scooter scooterRenault = new Scooter("Renault", TypeCar.scuter, "Green", "2020",
-                                                engineScuterRenault, transmissionScuterRenault, chassisScuter);
+                new Engine(15, 0.4, TypeEnine.gas, 45716334),
+                new Transmission(TypeTransmission.variableTransmission, 0, Manufacturer.renault),
+                new Chassis(2, "545486156", 0.15));
 
             passengerCarFord.Print();
             truckRenault.Print();
