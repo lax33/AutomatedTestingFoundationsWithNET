ba19ae7a2b1136d5fb503755bd56701a62d8a4d0
lax33 <lax33@tut.by>
2022-06-17 15:11:43 +0300

    1

--- ObjectOrientedDesignPrinciples/AveragePrice.cs
+++ ObjectOrientedDesignPrinciples/AveragePrice.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
 
 namespace ObjectOrientedDesignPrinciples
 {
     public class AveragePrice : ICommand
     {
-        private int averagePrice;
+        private int amountPrice;
+
         public void ExecuteCom(Cars cars)
         {
+            foreach (var car in cars.ArrayCars)
+            {
+                amountPrice += car.Price;
+            }
 
+            Console.WriteLine($"The average car's cost - {amountPrice / cars.ArrayCars.Count}");
         }
     }
 }
--- ObjectOrientedDesignPrinciples/AveragePriceType.cs
+++ ObjectOrientedDesignPrinciples/AveragePriceType.cs
@@ -5,9 +5,8 @@ namespace ObjectOrientedDesignPrinciples
 {
     public class AveragePriceType : ICommand
     {
-        private List<string> typeCar;
+        private Dictionary<string, int> typeCar_amountCarType;
         private double amountPriceType;
-       
 
         //public AveragePriceType(List<Car> cars)
         //{
@@ -19,14 +18,12 @@ namespace ObjectOrientedDesignPrinciples
             int amountCarType = 0; // number of car type
             foreach (var car in cars.ArrayCars)
             {
-                string typecar = car.Type; //выбираем текущий тип с которым будем работать 
+                string typecar = car.Type; //выбираем текущий тип с которым будем работать
 
-                foreach (var VARIABLE in typeCar) //массив с типами скоторыми уже работали 
+                foreach (var VARIABLE in typeCar_amountCarType) //массив с типами скоторыми уже работали
                 {
-                    if (typecar != VARIABLE) // cheсk for do we working with this type?  
+                    if (typecar != VARIABLE.Key) // cheсk for do we worked with this type?
                     {
-                        typeCar.Add(car.Type); // if we do not worked with this type, adding to  'typeCar' array
-
                         foreach (var ca in cars.ArrayCars)
                         {
                             if (typecar == ca.Type) // search in 'cars' array and add price
@@ -35,8 +32,10 @@ namespace ObjectOrientedDesignPrinciples
                                 amountPriceType += ca.Price;
                             }
                         }
+                        typeCar_amountCarType.Add(typecar, amountCarType); // if we do not worked with this type, adding to  'typeCar' array
                         amountPriceType /= amountCarType;
                         Console.WriteLine($"The average cost of '{typecar}' - {amountPriceType / amountCarType}");
+                        amountCarType = 0;
                     }
                     else
                     {
@@ -44,7 +43,7 @@ namespace ObjectOrientedDesignPrinciples
                     }
                 }
             }
-            typeCar.Clear(); //cleaning array for using later
+            //typeCar.Clear(); //cleaning array for using later
         }
     }
 }
