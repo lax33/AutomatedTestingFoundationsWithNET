61bf045063267f360d31ca6c7cb24e688f462d54
lax33 <lax33@tut.by>
2022-07-05 17:26:12 +0300

    add project unit test

---
+++ OOP/UnitTest1.cs
@@ -0,0 +1,31 @@
+using ObjectOrientedDesignPrinciples;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ObjectOrientedDesignPrinciples.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void ExecuteComTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace OOP
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Cars cars = null;
+
+            CountTypes types = new();
+
+        }
+    }
+}
---
+++ ObjectOrientedDesignPrinciplesTests/CountTypesTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObjectOrientedDesignPrinciples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedDesignPrinciples.Tests
+{
+    [TestClass()]
+    public class CountTypesTests
+    {
+        [TestMethod()]
+        public void ExecuteComTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
