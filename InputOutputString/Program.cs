fb6d4fbd6317a3bed47c44c2d215abaff4879cb2
lax33 <lax33@tut.by>
2022-07-18 16:56:01 +0300

    add new methods

--- InputOutputString/Program.cs
+++ InputOutputString/Program.cs
@@ -10,12 +10,17 @@ namespace InputOutputString
 
             char[] arr = SortChars(line); // result of sorting
 
-            // output to console
+            // output to console the new string 
             for (int j = 0; j < arr.Length; j++)
             {
                 if (arr[j] != '\0')
                 { Console.Write(arr[j]); }
             }
+
+            Console.WriteLine();
+            Console.WriteLine(AmountDiffirentChar(arr));
+
+            Console.WriteLine(AmountMaxEquelChar(arr));
 
             Console.ReadLine();
         }
@@ -47,6 +52,102 @@ namespace InputOutputString
             }
 
             return arr;
+        }
+
+        private static int AmountDiffirentChar(char[] line)
+        {
+            int diff = 0;
+            int numberLine = line.Length;
+            char[] arr = new char[numberLine];
+
+            for (int i = 0; i < numberLine; i++)
+            {
+                bool coincidence = false; // repeating character indicator
+                char ch = line[i];
+                for (int j = 0; j < numberLine; j++)
+                {
+                    if (i != j)
+                    {
+                        if (ch == arr[j])
+                        {
+                            coincidence = true;
+                        }
+                    }
+                }
+                if (!coincidence)
+                {
+                    diff++;
+                }
+            }
+
+            return diff ;
+        }
+
+        private static int AmountMaxEquelChar(char[] line)
+        {
+            int eqChar=0;
+            int numberLine = line.Length;
+            char[] arr = new char[numberLine];
+
+            for (int i = 0; i < numberLine; i++)
+            {
+                int r;
+                bool coincidence = false; // repeating character indicator
+                char ch = line[i];
+                
+                if (int.TryParse(ch.ToString(), out r)) continue;
+
+                for (int j = 0; j < numberLine; j++)
+                {
+                    if (i != j)
+                    {
+                        if (ch == arr[j])
+                        {
+                            eqChar++;
+                        }
+                    }
+                }
+                //if (coincidence)
+                //{
+                //    eqChar++;
+                //}
+            }
+
+            return eqChar ;
+        }
+
+        private static int AmountMaxEquelNumber(char[] line)
+        {
+            int eqNum=0;
+
+            int numberLine = line.Length;
+            char[] arr = new char[numberLine];
+
+            for (int i = 0; i < numberLine; i++)
+            {
+                int r;
+                bool coincidence = false; // repeating character indicator
+                char ch = line[i];
+
+                if (!int.TryParse(ch.ToString(), out r)) continue;
+
+                for (int j = 0; j < numberLine; j++)
+                {
+                    if (i != j)
+                    {
+                        if (ch == arr[j])
+                        {
+                            coincidence = true;
+                        }
+                    }
+                }
+                if (coincidence)
+                {
+                    eqNum++;
+                }
+            }
+
+            return eqNum;
         }
     }
 }
